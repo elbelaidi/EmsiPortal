0f9cfe809fd3bcc21e62354e8672b1ffed758db1
SimoHb7 <mohamedlahbari114@gmail.com>
2025-05-29 15:32:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import path from 'path';
 import fs from 'fs';
 const app = express();
 
+
 // Enable CORS for all routes
 app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:8080'],
